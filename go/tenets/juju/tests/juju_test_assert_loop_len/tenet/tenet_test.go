8973a98d40ce4181b0faf5be72769af91b471960
waigani <waigani@gmail.com>
2015-12-06 16:50:55 -0800

    Improve loop assert tenet
    Avoid false positives. Move example app to
    https://github.com/waigani/myAwesomeNetApp for demo.

--- go/tenets/juju/tests/juju_test_assert_loop_len/tenet/tenet_test.go
+++ go/tenets/juju/tests/juju_test_assert_loop_len/tenet/tenet_test.go
@@ -31,32 +31,45 @@ var _ = gc.Suite(&suite{})
 func (s *suite) SetUpTest(c *gc.C) {
 	s.Tenet = loop.New()
 	s.TenetSuite.SetUpTest(c)
-
 }
 
 func (s *suite) TestForLoop(c *gc.C) {
 
 	files := []string{
-		"example/network_test.go",
+		"example/bad_test.go",
 	}
 
 	expectedIssues := []tt.ExpectedIssue{
 		{
-			Filename: "example/network_test.go",
-			Text:     "\tfor p, aval := range server.Ports() {",
+			Filename: "example/bad_test.go",
+			Text:     "\tfor _, s := range list() {",
 			Comment: `
 Even if you assert the length of the result of this call before iterating
 over it, you cannot guarantee the result will be the same each time you call
-it. Thus, you cannot be sure that the asserts within the for loop will get
-run. Please assign the result of the call to a variable, assert the expected
-length of the variable and then loop over it.`[1:]}, {
-			Filename: "example/network_test.go",
-			Text:     "\tfor _, p := range portSlice {",
-			Comment:  "The asserts within this loop may never get run. The length of the collection being looped needs to be asserted.",
+it. You cannot be sure that the asserts within the for loop will get
+run. Please assign the result of list() to a variable, assert the expected
+length of the variable and then loop over that.`[1:],
 		}, {
-			Filename: "example/network_test.go",
-			Text:     "\tfor _, aval := range server.Ports() {",
-			Comment:  "Here also, you can't gurantee the length of the call's result.",
+			Filename: "example/bad_test.go",
+			Text:     "\tfor _, cont := range strings.Split(string(content), \"\\n\") {",
+			Comment:  `Here also, you can't gurantee the length of Split()'s result.`,
+		}, {
+			Filename: "example/bad_test.go",
+			Text:     "\tfor _, s := range a {",
+			Comment:  `The asserts within this loop may never get run. The length of "a" needs to be asserted.`,
+		},
+		{
+			Filename: "example/bad_test.go",
+			Text:     "\tfor _, s := range a {",
+			Comment:  `The length of "a" needs to be asserted also`,
+		}, {
+			Filename: "example/bad_test.go",
+			Text:     "\tfor _, s := range a {",
+			Comment:  `The length of "a" needs to be asserted also`,
+		}, {
+			Filename: "example/bad_test.go",
+			Text:     "\tfor _, s := range a {",
+			Comment:  `The length of "a" needs to be asserted also`,
 		},
 	}
 
